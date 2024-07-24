120a7522228727fc58c5abd51c74c4b2c6ea9690
freetimezp <kraftukrnet@ukr.net>
2024-07-24 14:02:21 +0300

    09. js-portfolio-105-space. Scrollbar. Scrollup btn. Active link on scroll.

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,10 +55,35 @@ const travelSwiper = new Swiper(".travel__swiper", {
 });
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+    const scrollup = document.getElementById("scroll-up");
 
+    this.scrollY >= 350 ? scrollup.classList.add("show-scroll") : scrollup.classList.remove("show-scroll");
+};
+
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
 
+const scrollActive = () => {
+    const scrollDown = window.scrollY;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 58;
+        const sectionId = current.getAttribute('id');
+        const sectionsClass = document.querySelector(".nav__menu a[href*=" + sectionId + "]");
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add("active-link");
+        } else {
+            sectionsClass.classList.remove("active-link");
+        }
+    });
+}
+
+window.addEventListener('scroll', scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
